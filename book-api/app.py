f10225b5521c2cce7cab57370bf89d72637929d3
Rahul Kumar <99110166+notahackerrr@users.noreply.github.com>
2022-05-01 13:36:50 +0530

    Add files via upload

---
+++ book-api/app.py
@@ -0,0 +1,22 @@
+from flask import Flask, jsonify
+from flask_restful import Api, Resource, reqparse
+import pandas as pd
+import json
+
+app = Flask(__name__)
+api = Api(app)
+
+
+class Books(Resource):
+    def get(self):
+        f = open('records.json')
+        data = json.load(f)
+        return data
+
+
+
+
+api.add_resource(Books, '/books')
+
+if __name__ == '__main__':
+    app.run()
